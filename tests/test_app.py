13e01697e5c437e6b1045f611da197c0d87cad47
Ali Rios <ali.rios@gmail.com>
2021-03-15 20:19:37 -0300

    test CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ def test_app_is_created(app):
     assert app.name == 'delivery.app'
 
 def test_config_is_loaded(config):
-    assert config["DEBUG"] is False # False
+    assert config["DEBUG"] is False
 
 def test_request_returns_404(client):
     assert client.get("/not_found").status_code == 404
